db97356336e287196fe9504f9120dd6aa0447dbd
Roufujjaman <rrhtcode@gmail.com>
2025-05-03 18:42:41 +0600

    first commit

---
+++ module_01/dist/1.4.js
@@ -0,0 +1,20 @@
+"use strict";
+// Basic Data Type
+// string
+let firstName = 'typescript';
+// numbr
+let id = 319;
+// boolean
+let isHuman = false;
+// undefined 
+let x = undefined;
+// numm
+let n = null;
+let d;
+// will be inferred as any type
+// Array
+let language = ['JS', 'TS', 'Python'];
+let digits = [1, 2, 3, 4];
+// tuple 
+let coordinates = [1, 5];
+let ageName = [100, 'Mr. R'];
---
+++ module_01/src/1.4.ts
@@ -0,0 +1,28 @@
+// Basic Data Type
+
+// string
+let firstName: string = 'typescript';
+// numbr
+let id: number = 319;
+// boolean
+let isHuman: boolean = false;
+// undefined 
+let x: undefined = undefined;
+// numm
+let n: null = null;
+
+
+let d;
+// will be inferred as any type
+
+
+// Array
+let language: string[] = ['JS', 'TS', 'Python'];
+let digits: number[] = [1, 2, 3, 4];
+
+// tuple 
+let coordinates: [number, number] = [1, 5];
+let ageName: [number, string, boolean] = [100, 'Mr. R', true];
+
+
+
