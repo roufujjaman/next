f85cdaffae1e1155ec8ca9bd1c4cd0e13df3a367
Roufujjaman <rrhtcode@gmail.com>
2025-05-07 23:43:29 +0600

    07 05 2025

---
+++ module_04/solution.ts
@@ -0,0 +1,79 @@
+function formatString(input: string, toUpper?: boolean): string {
+    if (!toUpper) {
+        return input.toLowerCase();
+    } else {
+        return input.toUpperCase();
+    }
+}
+
+type Item = {
+    title: string;
+    rating: number;
+}
+
+function filterByRating(items: Item[]): Item[] {
+    const higherRatedBooks: Item[] = items.filter((item: Item) => item.rating >= 4);
+    return higherRatedBooks;
+}
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    const newArray: T[] = [];
+    arrays.forEach((array: T[]) => newArray.push(...array));
+    return newArray;
+}
+
+class Vehicle {
+    private make: string;
+    private year: number;
+
+    constructor(make: string, year: number){
+        this.make = make;
+        this.year = year;
+    }
+
+    getInfo(): string {
+        return `Make: ${this.make}, Year: ${this.year}`;
+    }
+}
+
+class Car extends Vehicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string){
+        super(make, year);
+        this.model = model;
+    }
+
+    getModel(): string {
+        return `Model: ${this.model}`;
+    }
+}
+
+function processValue(value: string | number): number {
+    if (typeof value === 'string'){
+        return value.length;
+    } else {
+        return value * 2;
+    }
+}
+
+interface Product {
+    name: string;
+    price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+    let product: Product | null = null;
+    products.forEach((item: Product) => {
+        if (product) {
+            if (item.price > product.price){
+                product = item;
+            }
+        } else {
+            product = item;
+        }
+    })
+
+    return product;
+}
+
